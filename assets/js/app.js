d4d71a8b00237af45c32c795f2958b49bd1e7d01
Allison van Gemert <avangemert@apple.com>
2018-08-06 13:29:48 -0500

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,89 @@
-// @TODO: YOUR CODE HERE!
+// Chart params
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin ={
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+var file = "https://raw.githubusercontent.com/avangemert/d3/master/assets/data/data.csv"
+d3.csv(file).then(successHandle, errorHandle);
+
+function errorHandle(error){
+    throw err;
+}
+
+function successHandle(healthData){
+
+    // Print the healthData
+    console.log(healthData);
+
+    // Parse Data/Cast as floats
+    healthData.forEach(function(data){
+        data.poverty = parseFloat(data.poverty);
+        data.obesity = parseFloat(data.obesity);
+    });
+
+    // Create scale functions
+    var xLinearScale = d3.scaleLinear()
+        .domain([8, d3.max(healthData, d => d.poverty)])
+        .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(healthData, d => d.obesity)])
+        .range([height, 0]);
+
+    // Create axis functions
+    var bottomAxis = d3. axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Append Axes to the chart
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    // Create Circles
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(healthData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.obesity))
+    .attr("r", "10")
+    .attr("fill", "blue")
+    .attr("opacity", ".5");
+
+    // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("class", "aText")
+        .text("Obesity (%)");
+
+        
+
+
+
+
+    
+}
